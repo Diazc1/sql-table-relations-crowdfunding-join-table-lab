b3dafd6d8868a7fc0895bdddb64b39790e10a324
Cathy Diaz <Cathydiaz14@gmail.com>
2021-03-03 11:02:19 -0800

    commit tree 8a0851ed22536aa1436749d9f22ce27886e2a30e

---
+++ lib/insert.sql
@@ -0,0 +1,68 @@
+INSERT INTO users (name, age) VALUES ('Maria', 16);
+INSERT INTO users (name, age) VALUES ('Sophie', 18);
+INSERT INTO users (name, age) VALUES ('Cathy', 22);
+INSERT INTO users (name, age) VALUES ('Lemuel', 24);
+INSERT INTO users (name, age) VALUES ('Layla', 25);
+INSERT INTO users (name, age) VALUES ('Ivan', 26);
+INSERT INTO users (name, age) VALUES ('Oscar', 30);
+INSERT INTO users (name, age) VALUES ('Evelyn', 34);
+INSERT INTO users (name, age) VALUES ('Nicole', 36);
+INSERT INTO users (name, age) VALUES ('Penelope', 39);
+INSERT INTO users (name, age) VALUES ('Martha', 43);
+INSERT INTO users (name, age) VALUES ('Maria', 45);
+INSERT INTO users (name, age) VALUES ('Giselle', 46);
+INSERT INTO users (name, age) VALUES ('Bailey', 50);
+INSERT INTO users (name, age) VALUES ('Bacilia', 55);
+INSERT INTO users (name, age) VALUES ('Gloria', 56);
+INSERT INTO users (name, age) VALUES ('Richie', 57);
+INSERT INTO users (name, age) VALUES ('Annete', 58);
+INSERT INTO users (name, age) VALUES ('Jason', 60);
+INSERT INTO users (name, age) VALUES ('Wonka', 62);
+
+
+
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('How to feed a puppy', 'Dogs', 500, '03-03-2021', '04-03-2021');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('How to feed a kitten', 'Cats', 500, '02-25-2021', '03-25-2021');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('How to feed a pony', 'Horses', 1000, '05-01-2021', '10-01-2021');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('First time pet owners', 'Animals', 200, '06-01-2021', '08-01-2021');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('How to eat better', 'Healthy', 300, '01-01-2021', '03-01-2021');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Getting over anxiety', 'Mental Health', 650, '05-08-2021', '06-20-2021');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Insomnia', 'Sleeping', 700, '02-01-2021', '04-20-2021');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('How to buy a home', 'Homes', 900, '05-01-2021', '07-05-2021');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('How to invest', 'Investments', 950, '03-01-2021', '08-02-2021');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Relaxation', 'Relax', 400, '03-06-2021', '05-26-2021');
+
+
+
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
+(1, 16.00, 5, 9),
+(2, 18.00, 8, 3),
+(3, 15.00, 9, 7),
+(4, 18.00, 2, 4),
+(5, 92.00, 7, 6),
+(6, 20.00, 8, 3),
+(7, 22.00, 7, 2),
+(8, 25.00, 1, 3),
+(9, 28.00, 2, 5),
+(10, 30.00, 6, 5),
+(11, 32.00, 5, 8),
+(12, 36.00, 7, 1),
+(13, 20.00, 7, 6),
+(14, 50.00, 7, 6),
+(15, 12.00, 3, 2),
+(16, 15.00, 6, 7),
+(17, 15.00, 7, 8),
+(18, 19.00, 7, 3),
+(19, 25.00, 8, 5),
+(20, 26.00, 7, 8),
+(21, 65.00, 5, 2),
+(22, 45.00, 2, 6),
+(23, 23.00, 3, 4),
+(24, 18.00, 6, 9),
+(25, 42.00, 9, 1),
+(26, 35.00, 5, 4),
+(27, 65.00, 1, 4),
+(28, 32.00, 5, 4),
+(29, 25.00, 1, 9),
+(30, 34.00, 4, 6);
